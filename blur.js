d51fb83c52365effb52047c5a7276e2a72e8fa13
jacobk999 <jababakoshy@gmail.com>
2022-12-03 18:10:37 -0500

    add winter assets

--- blur.js
+++ blur.js
@@ -1,20 +1,28 @@
-import { mkdir, readdir, rm, writeFile } from 'fs/promises';
-import { createRequire } from 'module';
+/**
+ * Copyright (c) Statsify
+ *
+ * This source code is licensed under the GNU GPL v3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ * https://github.com/Statsify/statsify/blob/main/LICENSE
+ */
+
 import { canvasRGBA } from "stackblur-canvas";
+import { createRequire } from "node:module";
+import { mkdir, readdir, rm, writeFile } from "node:fs/promises";
 const require = createRequire(import.meta.url);
-const { Canvas, loadImage } = require('canvas');
+const { Canvas, loadImage } = require("canvas");
 
-await mkdir('./out', { recursive: true })
-await rm('./out/backgrounds', { force: true, recursive: true })
-await mkdir('./out/backgrounds', { recursive: true })
+await mkdir("./out", { recursive: true });
+await rm("./out/backgrounds", { force: true, recursive: true });
+await mkdir("./out/backgrounds", { recursive: true });
 
-const backgrounds = await readdir('backgrounds');
+const backgrounds = await readdir("backgrounds");
 
-const renderBackgrounds = backgrounds.map(async backgroundPath => {
+const renderBackgrounds = backgrounds.map(async (backgroundPath) => {
   const background = await loadImage(`./backgrounds/${backgroundPath}`);
 
-  const canvas = new Canvas(background.width, background.height, 'image');
-  const ctx = canvas.getContext('2d');
+  const canvas = new Canvas(background.width, background.height, "image");
+  const ctx = canvas.getContext("2d");
   ctx.drawImage(background, 0, 0);
 
   canvasRGBA(canvas, 0, 0, canvas.width, canvas.height, 13);
